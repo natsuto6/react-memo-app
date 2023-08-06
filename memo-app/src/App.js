680a5671a6acc2bb82437139d01e4f56ef9d1612
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2023-08-06 18:56:18 +0900

    メモ一覧と詳細の表示、新規メモの追加機能の実装

--- memo-app/src/App.js
+++ memo-app/src/App.js
@@ -1,8 +1,28 @@
+import { useState } from 'react';
+import MemoList from './components/MemoList';
+import MemoDetail from './components/MemoDetail';
+import useLocalStorage from './hooks/useLocalStorage';
+
 function App() {
+  const [memos, setMemos] = useLocalStorage('memos', []);
+  const [selectedMemoIndex, setSelectedMemoIndex] = useState(null);
+
+  const handleMemoSelect = (index) => {
+    setSelectedMemoIndex(index);
+  };
+
+  const handleMemoAdd = () => {
+    const newMemo = { text: '新規メモ'};
+    setMemos([...memos, newMemo]);
+    setSelectedMemoIndex(memos.length);
+  };
+
   return (
-    <div className="App">
+    <>
       <h1>メモアプリ</h1>
-    </div>
+      <MemoList memos={memos} onMemoSelect={handleMemoSelect} onMemoAdd={handleMemoAdd} />
+      {selectedMemoIndex !== null && <MemoDetail memo={memos[selectedMemoIndex]} />}
+    </>
   );
 }
 
