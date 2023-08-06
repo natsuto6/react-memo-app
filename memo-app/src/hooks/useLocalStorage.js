5cf2215038e6f2111d83318850249ce761b585b7
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2023-08-06 11:05:41 +0900

    useLocalStorageカスタムフックの作成

---
+++ memo-app/src/hooks/useLocalStorage.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+function useLocalStorage(key, initialValue) {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value) => {
+    try {
+      window.localStorage.setItem(key, JSON.stringify(value));
+      setStoredValue(value);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
+
+export default useLocalStorage;
