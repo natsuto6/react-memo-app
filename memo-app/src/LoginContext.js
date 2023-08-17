ad697dca036f7d306b2253e176459b3fafe4fb59
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2023-08-17 14:56:23 +0900

    ログイン状態管理用のContextとカスタムフックを作成

---
+++ memo-app/src/LoginContext.js
@@ -0,0 +1,20 @@
+import { createContext, useContext, useState } from "react";
+
+const LoginContext = createContext();
+
+export function LoginProvider({ children }) {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const login = () => setLoggedIn(true);
+  const logout = () => setLoggedIn(false);
+
+  return (
+    <LoginContext.Provider value={{ loggedIn, login, logout}}>
+      {children}
+    </LoginContext.Provider>
+  );
+}
+
+export function useLogin() {
+  return useContext(LoginContext);
+}
